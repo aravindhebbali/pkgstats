1497d45c36b1dfc2fd1c206456734d4a96befd54
rsquaredin <rsquaredin@gmail.com>
2019-04-06 19:15:22 +0530

    remove scriptr

--- schedule.R
+++
@@ -1,8 +0,0 @@
-install.packages('taskscheduleR')
-library(taskscheduleR)
-library(here)
-myscript <- "J:/R/Others/pkgstats/country_wise.R"
-taskscheduler_create(taskname = "cran_downloads_new", rscript = myscript, 
-                     schedule = "DAILY", starttime = "22:27")
-
-
